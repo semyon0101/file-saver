19d6369f2fd70ddfe21acb6ffeb7772125e168bc
semyon0101 <k.semyon.p@gmail.com>
2025-12-24 17:02:59 +0300

    I tried to make stream to upload file

--- app/ui/buttonUploadFile.tsx
+++ app/ui/buttonUploadFile.tsx
@@ -1,8 +1,43 @@
 'use client';
 
-import { useActionState } from 'react';
+import { FormEvent, useState } from 'react';
 import { uploadFilesasdfsdfsdfsd, State } from '@/app/lib/actions';
 
+export default function ButtonUploadFile() {
+	const [status, setStatus] = useState<string>('Idle');
+
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setStatus('Uploading...');
+
+		const formData = new FormData(e.currentTarget);
+
+
+		const response = await fetch('/api/upload', {
+			method: 'POST',
+			body: formData,
+		});
+		console.log("123");
+
+		if (!response.ok) {
+			console.error("Something went wrong");
+			setStatus('Error');
+
+		} else {
+			setStatus('Success!');
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type="file" name="file" /> <br />
+			<button type="submit">Secure Upload</button>
+			<p>Status: {status}</p>
+		</form>
+	);
+}
+
+/*
 export default function ButtonUploadFile() {
 	const initialState: State = {};
 	const [state, formAction] = useActionState(uploadFilesasdfsdfsdfsd, initialState);
@@ -23,3 +58,4 @@ export default function ButtonUploadFile() {
 		</form>
 	);
 }
+*/
